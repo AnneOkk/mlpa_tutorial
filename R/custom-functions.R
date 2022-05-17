7eba82ac26656653f5551c493a6c83de22c3a7fd
Anne <Your address email (eg. you@example.com)>
2022-05-17 22:13:21 +0200

    added config material

---
+++ R/custom-functions.R
@@ -0,0 +1,10 @@
+# Read in data ------------------------------------------------------------
+read_data_excel <- function(rel_directory, pattern) {
+    files <- dir(paste0(dirname(getwd()),"/", rel_directory), pattern = pattern, full.names = FALSE)
+    df_list <- vector("list", length(files))
+    for (fname in files) {
+        df_list[[fname]] <- read_excel(paste0(dirname(getwd()),"/", rel_directory ,fname))
+    }
+    names(df_list) <- paste0("", gsub(pattern,"",names(df_list)))
+    return(df_list)
+}
