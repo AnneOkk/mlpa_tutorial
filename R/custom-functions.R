5ab75ebdd7f5f71ce90f32eea25e68c87549bff1
Anne Kleine <annekleine@Annes-Air.lan>
2022-07-20 17:18:38 +0200

    new analyses and explanation

--- R/custom-functions.R
+++ R/custom-functions.R
@@ -1,4 +1,19 @@
-# Read in data ------------------------------------------------------------
+library(haven)
+library(rlang)
+library(tidyverse)
+library(foreign)
+library(dplyr)
+library(naniar)
+library(stringr)
+library(sjlabelled)
+library(zoo)
+library(lubridate)
+library(sf)
+library(labelled)
+library(glca)
+
+
+# Read in data excel ------------------------------------------------------------
 read_data_excel <- function(rel_directory, pattern) {
     files <- dir(paste0(dirname(getwd()),"/", rel_directory), pattern = pattern, full.names = FALSE)
     df_list <- vector("list", length(files))
@@ -9,6 +24,18 @@ read_data_excel <- function(rel_directory, pattern) {
     return(df_list)
 }
 
+## code to prepare `df` dataset
+
+# Read in data sav ------------------------------------------------------------
+read_data_sav <- function(rel_directory, pattern) {
+  files <- dir(paste0(dirname(getwd()),"/", rel_directory), pattern = pattern, full.names = FALSE)
+  df_list <- vector("list", length(files))
+  for (fname in files) {
+    df_list[[fname]] <- haven::read_sav(paste0(dirname(getwd()),"/", rel_directory ,fname))
+  }
+  names(df_list) <- paste0("", gsub(pattern,"",names(df_list)))
+  return(df_list)
+}
 
 # mean_median -------------------------------------------------------------
 
@@ -30,7 +57,54 @@ mysummary <- function(x,npar=TRUE,print=TRUE) {
     return(result)
 }
 
-# subby function
-subby <- function(a, b) {
-  a - b
+
+# Correlation Table -------------------------------------------------------
+
+corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper", "lower", "none"),
+                    result=c("none", "html", "latex")){
+  #Compute correlation matrix
+  require(Hmisc)
+  x <- as.matrix(x)
+  correlation_matrix<-rcorr(x, type=method[1])
+  R <- correlation_matrix$r # Matrix of correlation coeficients
+  p <- correlation_matrix$P # Matrix of p-value
+
+  ## Define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    ")))
+
+  ## trunctuate the correlation matrix to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
+
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
+  diag(Rnew) <- paste(diag(R), " ", sep="")
+  rownames(Rnew) <- colnames(x)
+  colnames(Rnew) <- paste(colnames(x), "", sep="")
+
+  ## remove upper triangle of correlation matrix
+  if(removeTriangle[1]=="upper"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+
+  ## remove lower triangle of correlation matrix
+  else if(removeTriangle[1]=="lower"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[lower.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+
+  else if(removeTriangle[1]=="none"){
+    Rnew <- as.matrix(Rnew)
+    Rnew <- as.data.frame(Rnew)
+  }
+
+  ## remove last column and return the correlation matrix
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  if (result[1]=="none") return(Rnew)
+  else{
+    if(result[1]=="html") print(xtable(Rnew), type="html")
+    else print(xtable(Rnew), type="latex")
+  }
 }
--- analysis/analyses.md
+++ analysis/analyses.md
@@ -27,7 +27,7 @@ summary(cars)
 
 You can also embed plots, for example:
 
-![](github_doc_files/figure-gfm/pressure-1.png)<!-- -->
+![](analyses_files/figure-gfm/pressure-1.png)<!-- -->
 
 Note that the `echo = FALSE` parameter was added to the code chunk to
 prevent printing of the R code that generated the plot.
